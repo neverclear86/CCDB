4f9bffa03cdf902e536e244e142b3cc3f5ec18e9
neverclear86 <neverclear86sab@gmail.com>
2017-05-27 18:26:45 +0900

    Gradleわかった

--- src/net/neverclear/ccdb/Reciever.java
+++ src/net/neverclear/ccdb/Reciever.java
@@ -8,18 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Reciever extends HttpServlet {
-	
-	
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.doPost(req, resp);
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println(req.getSession().getId());
-	}
-	
-	
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println(req.getSession().getId());
+    }
+
 }
